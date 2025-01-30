4868cc1ba83f78dbae151bc8edccc36c0a63c453
FernandoAlcala <166526484+FernandoAlcala@users.noreply.github.com>
2025-01-29 18:10:42 -0600

    Update multiple endpoints

--- Fast/main.py
+++ Fast/main.py
@@ -2,7 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-#Ruta o EndPoint
+#EndPoint home
 @app.get('/')
 def home():
 	return {'hello':'Hello FastAPI!'}
+
+#EndPoint promedio
+@app.get('/promedio')
+def promedio():
+	return 10.5
+
+#EndPoint parámetro obligatorio
+@app.get('/usuario/{id}')
+def consultausuario(id:int):
+	#Caso ficticio de busqueda en BD
+	return {"Se encontró el usuario":id}
